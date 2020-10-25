89afb41deb2c8c856fb631581f25598803c5b410
Nicolás <nikolas6060@gmail.com>
2020-10-25 11:09:08 -0500

    Primer commit

---
+++ src/main/java/com/capacitacion2/capacitacion2/App.java
@@ -0,0 +1,13 @@
+package com.capacitacion2.capacitacion2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
